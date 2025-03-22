26866bb5df474f95f778f7f9a263d72052ebca85
Noor-E-Kainat <kainathasan92@gmail.com>
2025-03-22 19:44:40 +0600

    flex section on index html, css and js

---
+++ index.js
@@ -0,0 +1,9 @@
+// Show alert when user clicks "Learn More" button
+function showAlert() {
+    alert("Welcome to GreenLeaf Shop! Explore our wide collection of beautiful plants.");
+}
+
+// Show alert when user clicks "Our Story" button
+function moreInfo() {
+    alert("GreenLeaf Shop has been helping homes go green since 2020. We love plants as much as you do!");
+}
